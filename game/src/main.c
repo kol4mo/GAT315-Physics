9278392e261098ec8ab12885684860d097868d53
Kol4mo <47543776+kol4mo@users.noreply.github.com>
2024-04-30 16:17:55 -0600

    updated to use linked lists

--- game/src/main.c
+++ game/src/main.c
@@ -1,14 +1,63 @@
+#include "body.h"
+#include "world.h"
 #include "raylib.h"
+#include "mathf.h"
+#include "raymath.h"
+
+#include <stdlib.h>
+#include <assert.h>
+
+#define MAX_BODIES 10000
+
 int main(void)
 {
-	InitWindow(800, 450, "raylib [core] example - basic window");
+	InitWindow(1280, 720, "particles");
+	SetTargetFPS(60);
+
+	int bodyCount = 0;
+
+	//game loop
 	while (!WindowShouldClose())
 	{
+		//update
+		float dt = GetFrameTime();
+		float fps = (float)GetFPS();
+
+		Vector2 position = GetMousePosition();
+		if (IsMouseButtonDown(0)) {
+			Body* body = CreateBody();
+			body->position = position;
+			body->velocity = CreateVector2(GetRandomFloatValue(-5, 5), GetRandomFloatValue(-5, 5));
+			bodyCount++;
+		}
+
+		//Render
 		BeginDrawing();
-		ClearBackground(RAYWHITE);
-		DrawText("Congrats! You created your thrid window!", 190, 200, 20, LIGHTGRAY);
+		ClearBackground(BLACK);
+
+		//stats
+		DrawText(TextFormat("FPS: %.2f (ms %.2fms)", fps, 1000/fps), 10, 10, 20, LIME);
+		DrawText(TextFormat("FRAME: %.4f", dt), 10, 30, 20, LIME);
+
+		DrawCircle((int)position.x, (int)position.y, 10, YELLOW);
+		//DrawCircle((int)position.x, (int)position.y, 5, BLACK);
+		//DrawCircleLines((int)position.x, (int)position.y, 10, YELLOW);
+		Body* body = bodies;
+		while (body) // do while we have a valid pointer, will be NULL at the end of the list
+		{
+			// update body position
+			body->position = Vector2Add(body->position, body->velocity);
+			// draw body
+			DrawCircle((int)(body->position.x), (int)body->position.y, 10, RED);
+
+			body = body->next; // get next body
+		}
+
 		EndDrawing();
 	}
+
+
 	CloseWindow();
+	free(bodies);
 	return 0;
 }
---
+++ game/src/world.c
@@ -0,0 +1,35 @@
+#pragma once
+#include "body.h"
+#include <stdlib.h>
+#include <assert.h>
+
+Body* bodies = NULL;
+int bodyCount = 0;
+
+ Body* CreateBody() {
+	 Body* body = (Body*)malloc(sizeof(Body));
+	 assert(body);
+	 body->prev = NULL;
+	 body->next = bodies;
+	 if (bodies) {
+		 bodies->prev = body;
+	 }
+	 bodies = body;
+	 bodyCount++;
+
+	 return body;
+}
+ void DestroyBody(Body* body) {
+	 assert(body);
+	 if (body->prev) {
+		 body->prev->next = body->next;
+	 }
+	 if (body->next) {
+		 body->next->prev = body->prev;
+	 }
+	 if (body == bodies) {
+		 bodies = body->next;
+	 }
+	 bodyCount--;
+	 free(body);
+}
